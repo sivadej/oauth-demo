52cbe00cf693d2bb976f3e3b4b04484dbdd3d378
Sivadej K <bombyk@gmail.com>
2020-05-23 00:04:36 -0800

    components and routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Router, Route } from 'react-router-dom';
+import history from './history';
+import UserProvider from './contexts/UserProvider';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import MenuBar from './components/menus/MenuBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          hello world
-        </p>
-      </header>
+      <Router history={history}>
+        <UserProvider>
+          <Route path='/' component={MenuBar}/>
+          <Route path='/' component={Profile}/>
+        </UserProvider>
+        <Route path='/' exact component={Home}/>
+      </Router>
     </div>
   );
 }
